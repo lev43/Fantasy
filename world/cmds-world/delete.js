cd6e1996e15f93fb6226c01523927899d144878a
lev43 <68313120+lev43@users.noreply.github.com>
2020-09-14 18:10:14 +0300

    turururu

--- world/cmds-world/delete.js
+++ world/cmds-world/delete.js
@@ -15,10 +15,12 @@ fs.readdir('./cmds-world/delete/', (err, files) => {
 });
 
 module.exports.run = async (world, message, args, player) => {
-	let cmd = commands.get(args[0]);
-	if (cmd) {
-		args.splice(0,1);
-		cmd.run(world, message, args, player);
+	if(message.member.roles.cache.find(role=>role.name=='Божество')){
+		let cmd = commands.get(args[0]);
+		if (cmd) {
+			args.splice(0,1);
+			cmd.run(world, message, args, player);
+		};
 	};
 };
 module.exports.help = {
