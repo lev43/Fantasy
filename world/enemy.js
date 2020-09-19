6d84bb4033f8c740f3cd54b42efffedcee06d869
lev43 <68313120+lev43@users.noreply.github.com>
2020-09-19 07:52:52 +0300

    update

--- world/enemy.js
+++ world/enemy.js
@@ -1,19 +1,28 @@
 class Enemy{
-	constructor(name, id, spawn_location){
+	constructor(name, id, spawn_location, type){
+		this.type=type;
 		this.name=String(name);
 		this.id=id;
 		this.spawnPoint=spawn_location.name;
 		this.location=spawn_location.name;
 	}
 }
-
+class Monster extends Enemy{
+	constructor(name, id, spawn_location){
+		super(name, id, spawn_location, "monster");
+	}
+	action(){
+		
+	}
+}
 class Player extends Enemy{
 	constructor(name, id, spawn_location){
-		super(name, id, spawn_location);
+		super(name, id, spawn_location, "player");
 	}
 }
 
 module.exports={
 	Enemy: Enemy,
-	Player: Player
+	Player: Player,
+	Monster: Monster
 };
